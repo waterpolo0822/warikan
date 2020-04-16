b6cef86e7ed602338bf0e377a71f139bf2715701
waterpolo0822 <gerge0822@gmail.com>
2020-04-17 00:04:45 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="割り勘計算機"/>
   </div>
 </template>
 
